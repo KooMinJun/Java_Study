b061badc3dc9eda2ef2ef2232363a217360e5628
Koo Min Jun <jooon0711@kookmin.ac.kr>
2016-11-09 01:51:02 +0900

    QuickCoding04 Thread 만들기 (Account)

---
+++ Mobile_Programming/src/QuickCoding04/testMain.java
@@ -0,0 +1,41 @@
+package QuickCoding04;
+
+public class testMain {
+	public static BankAccount account = new BankAccount();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Thread depositMan = new Thread(){
+			public void run(){
+				for(int i = 0; i< 10; i++){
+					account.add(10);
+					try{
+						Thread.sleep((int)(Math.random() * 1000));
+					}catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			
+		};
+		
+		Thread withdrawMan = new Thread(){
+			public void run(){
+				for(int i=0 ; i<10 ; i++){
+					account.delete(10);
+					try{
+						Thread.sleep((int)(Math.random() * 1000));
+					}catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		
+		depositMan.start();
+		withdrawMan.start();
+		
+	}
+
+}
